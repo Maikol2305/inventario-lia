0f4cce47dc0bba90c2518a81f19e508528dccc20
Maikol2305 <maikoljesus86@gmail.com>
2026-04-23 18:26:50 -0400

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  // Redirección forzada al dashboard
-  redirect("/dashboard");
-}
--- src/lib/resend.ts
+++
@@ -1,5 +0,0 @@
-import { Resend } from 'resend';
-
-// Clave de respaldo proporcionada para asegurar el funcionamiento inicial
-const apiKey = process.env.RESEND_API_KEY || "Maikoljesus15.";
-export const resend = new Resend(apiKey);
